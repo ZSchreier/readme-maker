8f8d2d477004c0604c1230c118bf10cffd031544
zschreier <zjschreier@gmail.com>
2023-10-24 15:45:44 -0500

    added the questions and a template literal for testing how it works

--- index.js
+++ index.js
@@ -1,14 +1,118 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  {
+    type: 'input',
+    message: 'What would you like your title to be?',
+    name: 'title'
+  },
+  {
+    type: 'input',
+    message: 'Give me your description:',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'List any install instructions:',
+    name: 'installInstructions'
+  },
+  {
+    type: 'input',
+    message: 'List any pertinent usage information:',
+    name: 'usageInfo'
+  },
+  {
+    type: 'input',
+    message: 'List any contribution guidelines:',
+    name: 'contribution'
+  },
+  {
+    type: 'input',
+    message: 'List any necessary test instructions:',
+    name: 'testInstructions'
+  },
+  {
+    type: 'list',
+    message: 'Which license is your project using:',
+    name: 'license',
+    choices: [
+      {name: 'MIT', value: 'https://choosealicense.com/licenses/mit/'},
+      {name: 'GNU GPLv3', value: 'https://choosealicense.com/licenses/gpl-3.0/'},
+      {name: 'Apache', value: 'https://choosealicense.com/licenses/apache-2.0/'}
+    ]
+  },
+  {
+    type: 'input',
+    message: `What's your Github username?`,
+    name: 'githubName'
+  },
+  {
+    type: 'input',
+    message: `What email do you want to provide for others?`,
+    name: 'description'
+  }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+function init() {
+  inquirer.prompt(questions)
+    .then(responses => {
+      const text = 
+`
+# ${title}
+![Static Badge](https://img.shields.io/badge/License-${license.name}-blue)
+
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage Infromation](#usage-information)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+${installInstructions}
+
+## Usage Information
+
+${usageInfo}
+
+## Contribution Guidelines
+
+${contribution}
+
+## Test Instructions
+
+${testInstructions}
+
+## License
+
+The license used for this project is [${license.name}](${license.value}).
+
+---
+
+## Questions
+
+Contact me on [Github](https://github.com/${githubName})
+
+Contact me via email at ${email}`;
+
+      fs.writeFile('README.md', text, (error) => {
+        if(error){
+          console.log('Something failed here');
+        }
+      })
+    })
+}
+
 init();
